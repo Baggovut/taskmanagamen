dd7840ebc08766d724aad4e23e42c9ef85b0b577
Baggovut <117596144+Baggovut@users.noreply.github.com>
2024-11-23 17:28:30 +0300

    First commit

---
+++ src/test/java/com/testtask/taskmanagement/TaskManagementSystemApplicationTests.java
@@ -0,0 +1,15 @@
+package com.testtask.taskmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+class TaskManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
